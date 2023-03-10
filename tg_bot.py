9e9de44a315078ba2dd6229e62528663ac8d711a
YuraML <malashtayuriy@gmail.com>
2023-03-10 20:39:49 +0400

    create images publication script

--- tg_bot.py
+++
@@ -1,9 +0,0 @@
-import os
-import telegram
-
-from dotenv import load_dotenv
-load_dotenv()
-
-bot = telegram.Bot(token=os.getenv('TG_TOKEN'))
-chat_id = os.getenv('TG_CHANNEL_ID')
-bot.send_document(chat_id=chat_id, document=open('images/nasa_apod_0.jpg', 'rb'))
---
+++ tg_publication_script.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import random
+import telegram
+import time
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+parser = argparse.ArgumentParser(
+    description='Скрипт позволяет публиковать фотографии космоса в telegram канале при помощи бота.')
+parser.add_argument('--publication_delay',
+                    help='Укажите временной промежуток публикации фото в часах.',
+                    default=4,
+                    type=int)
+args = parser.parse_args()
+publication_delay = args.publication_delay * 60 * 60
+
+images_path = os.path.join(os.getcwd(), 'images')
+tg_token = os.getenv('TG_TOKEN')
+chat_id = os.getenv('TG_CHANNEL_ID')
+bot = telegram.Bot(token=tg_token)
+pictures_in_dir = os.walk(images_path)
+
+while True:
+    for root, dirs, image_filenames in pictures_in_dir:
+        random.shuffle(image_filenames)
+        for filename in image_filenames:
+            image_path = os.path.join(images_path, filename)
+            bot.send_document(chat_id=chat_id, document=open(image_path, 'rb'))
+            time.sleep(publication_delay)
